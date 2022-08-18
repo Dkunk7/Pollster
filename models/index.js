8e22fe7d5714cc1298824b337155970c6d55bff0
Dylan K <drkunkel7@gmail.com>
2022-08-17 22:21:55 -0500

    user model more or less set up

---
+++ models/index.js
@@ -0,0 +1,12 @@
+// import models
+const User = require('./User');
+const Poll = require('./Poll');
+const Survey = require('./Survey');
+
+// Not sure exactly if/how these fit in (especially with associations)
+const Question = require('./Question');
+const Answer = require('./Answer');
+
+const { Model } = require('sequelize'); // Do I need this? Probably not
+
+// make the stupid lame annoying associations here :(
